f4a53b9c14e82569b845b4a684fe1e24a6043a64
王麻子 <2538208788@qq.com>
2019-01-22 18:15:02 +0800

    商品种类列表

--- pinyougou-parent/pinyougou-manager-web/src/main/webapp/js/service/typeTemplateService.js
+++ pinyougou-parent/pinyougou-manager-web/src/main/webapp/js/service/typeTemplateService.js
@@ -28,5 +28,10 @@ app.service('typeTemplateService',function($http){
 	//搜索
 	this.search=function(page,rows,searchEntity){
 		return $http.post('../typeTemplate/search.do?page='+page+"&rows="+rows, searchEntity);
-	}    	
+	}
+
+	//下拉列表数据
+	this.findTypeTemplateList = function () {
+		return $http.get("../typeTemplate/findTypeTemplateList.do");
+	}
 });
